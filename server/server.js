2714ea4df6bc784b81545cb85649bfd86855f688
Jared Thomas <jjthom87@yahoo.com>
2016-11-03 18:05:22 -0400

    starting homepage

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var PORT = 8000;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(methodOverride('_method'));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/static', express.static('client'));
+app.use(express.static('client'));
+
+app.get('/', function(req,res){
+	res.render('index');
+})
+
+app.listen(PORT, function(){
+  console.log("Listening on port", PORT);
+});
